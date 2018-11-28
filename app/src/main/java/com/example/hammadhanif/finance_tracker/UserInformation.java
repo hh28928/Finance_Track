d91bbb0bfe338b7cec0e590636c6e4912e8fe303
jihad <jshehad@masonlive.gmu.edu>
2018-11-28 17:21:27 -0500

    Merge branch 'master' of https://github.com/hh28928/Finance_Track
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/hammadhanif/finance_tracker/MenuActivity.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/hammadhanif/finance_tracker/UserInformation.java
+++ app/src/main/java/com/example/hammadhanif/finance_tracker/UserInformation.java
@@ -9,6 +9,8 @@ public class UserInformation {
     public String state;
     public String zip;
     public String country;
+    public String budget;
+    public String balance;
 
     public UserInformation(){
 
@@ -22,35 +24,79 @@ public class UserInformation {
         this.state = state;
         this.zip = zip;
         this.country = country;
+        this.budget = " ";
+        this.balance = " ";
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getStreet() {
         return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
     }
 
     public String getCity() {
         return city;
     }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public String getState() {
         return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 
     public String getZip() {
         return zip;
     }
 
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
     public String getCountry() {
         return country;
     }
-    
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
 }
