a1638f3d8733b7509cfd96cb9b713ac3337dea7a
= <hhanif2@masonlive.gmu.edu>
2018-11-28 15:33:52 -0500

    Bill Reminder Done!

--- app/src/main/java/com/example/hammadhanif/finance_tracker/BillReminderActivity.java
+++ app/src/main/java/com/example/hammadhanif/finance_tracker/BillReminderActivity.java
@@ -1,15 +1,56 @@
 package com.example.hammadhanif.finance_tracker;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BillReminderActivity extends AppCompatActivity {
 
+
+    String stName, stDate;
+    ListView listView;
+    ArrayAdapter myAdapter;
+    ArrayList<String> ToDO = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_reminder);
+
+        listView = (ListView) findViewById(R.id.myList);
+
     }
 
-    
-}
+
+    public void onClickAdd(View view) {
+        Intent addIntent = new Intent(this, NotifyMeActivity.class);
+        startActivityForResult(addIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            stName = data.getStringExtra("NAME");
+            stDate = data.getStringExtra("DATE");
+            String st_complete = stName + "  " + stDate;
+
+            ToDO.add(st_complete);
+
+            myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, ToDO);
+
+            listView.setAdapter(myAdapter);
+        }
+        }
+    }
